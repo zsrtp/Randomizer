7df8b418dc067033329fe2c4ef667b68ac510cac
AECX <maurice.henke@protonmail.com>
2020-12-12 05:37:27 +0100

    ACTR hash print for rando; updated & added d_stage stuff, updated strncmp in lst file (todo: eu/jp);

--- GameCube/include/main.h
+++ GameCube/include/main.h
@@ -1,4 +1,37 @@
+#pragma once
+#include <tp/d_stage.h>
+
+#include <cstdint>
+
+// Allows to transform data as bytes 1:1 from A<-->B and vice versa
+template<typename A, typename B>
+union typeTransform
+{
+    A a;
+    B b;
+};
+
 namespace mod
 {
+    uint8_t delayCounter;
+
+    uint8_t hashCounter;
+
     void main();
-}
+    // runs every frame
+    void run();
+
+    void onActorInit( libtp::tp::d_stage::dzxChunkTypeInfo* chunkTypeInfo );
+
+    uint16_t fletcher16( uint8_t* data, int32_t count );
+
+    void ( *fapGm_Execute_return )() = nullptr;
+    bool ( *actorInit_return )( void* mStatus_roomControl,
+                                libtp::tp::d_stage::dzxChunkTypeInfo* chunkTypeInfo,
+                                int32_t unk3,
+                                void* unk4 ) = nullptr;
+    bool ( *actorInit_always_return )( void* mStatus_roomControl,
+                                       libtp::tp::d_stage::dzxChunkTypeInfo* chunkTypeInfo,
+                                       int32_t unk3,
+                                       void* unk4 ) = nullptr;
+}     // namespace mod
--- GameCube/source/main.cpp
+++ GameCube/source/main.cpp
@@ -1,17 +1,106 @@
 #include "main.h"
 
+#include <display/console.h>
+#include <patch.h>
 #include <tp/JFWSystem.h>
+#include <tp/d_stage.h>
+#include <tp/f_ap_game.h>
 
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
 
 namespace mod
 {
     void main()
-    {
+    {     // Hook function that runs each frame
+        fapGm_Execute_return = libtp::patch::hookFunction( libtp::tp::f_ap_game::fapGm_Execute, run );
+
+        actorInit_return = libtp::patch::hookFunction(
+            libtp::tp::d_stage::actorInit,
+            []( void* mStatus_roomControl, libtp::tp::d_stage::dzxChunkTypeInfo* chunkTypeInfo, int32_t unk3, void* unk4 ) {
+                // pass relevant information to our custom event
+                onActorInit( chunkTypeInfo );
+                // Call original function
+                return actorInit_return( mStatus_roomControl, chunkTypeInfo, unk3, unk4 );
+            } );
+
+        actorInit_always_return = libtp::patch::hookFunction(
+            libtp::tp::d_stage::actorInit_always,
+            []( void* mStatus_roomControl, libtp::tp::d_stage::dzxChunkTypeInfo* chunkTypeInfo, int32_t unk3, void* unk4 ) {
+                // pass relevant information to our custom event
+                onActorInit( chunkTypeInfo );
+                // Call original function
+                return actorInit_always_return( mStatus_roomControl, chunkTypeInfo, unk3, unk4 );
+            } );
+
         libtp::tp::jfw_system::systemConsole->consoleEnabled = true;
-        libtp::tp::jfw_system::systemConsole->consoleLine[0].showLine = true;
-        strcpy( libtp::tp::jfw_system::systemConsole->consoleLine[0].line, "Hello World" );
-        return;
+        // libtp::tp::jfw_system::systemConsole->consoleLine[0].showLine = true;
+
+        libtp::display::setConsole( true, 25 );
+    }
+
+    void onActorInit( libtp::tp::d_stage::dzxChunkTypeInfo* chunkTypeInfo )
+    {
+        // Display info and create unique hash for this check
+        delayCounter = 0;
+
+        if ( hashCounter == 0 )
+        {
+            // First call of this function for this load so clear the console output
+            libtp::display::clearConsole( 0, 0 );
+        }
+
+        uint8_t numACTR = chunkTypeInfo->numChunks;
+        libtp::tp::d_stage::Actr* actor = reinterpret_cast<libtp::tp::d_stage::Actr*>( chunkTypeInfo->chunkDataPtr );
+
+        for ( uint8_t i = 0; i < numACTR; i++ )
+        {
+            if ( strncmp( actor[i].objectName, "tboxXX", 4 ) == 0 || strcmp( actor[i].objectName, "htpiece" ) == 0 )
+            {
+                // This actor is relevant to rando and thus should be hashed
+                uint16_t hash = fletcher16( reinterpret_cast<uint8_t*>( &actor[i] ), 0x20 );
+                uint8_t item = ( actor[i].rot[2] >> 8 );
+
+                sprintf( libtp::tp::jfw_system::systemConsole->consoleLine[hashCounter].line,
+                         "%04x: %02x %02x %04.4f %04.4f %04.4f",
+                         reinterpret_cast<uint32_t>( &actor[i] ),
+                         hash,
+                         item,
+                         actor[i].pos[0],
+                         actor[i].pos[1],
+                         actor[i].pos[2] );
+
+                hashCounter++;
+            }
+        }
+    }
+
+    void run()
+    {
+        // Do stuff
+        if ( hashCounter > 0 && delayCounter == 0xFF )
+        {
+            // Assume the load has ended and reset
+            hashCounter = 0;
+        }
+
+        delayCounter++;
+        // Call original game function
+        return fapGm_Execute_return();
+    }
+
+    uint16_t fletcher16( uint8_t* data, int32_t count )
+    {
+        uint16_t sum1 = 0;
+        uint16_t sum2 = 0;
+
+        for ( int32_t index = 0; index < count; ++index )
+        {
+            sum1 = ( sum1 + data[index] ) % 0xFF;
+            sum2 = ( sum2 + sum1 ) % 0xFF;
+        }
+
+        return ( sum2 << 8 ) | sum1;
     }
 }     // namespace mod
