65c2bc2e27429a04ba02a7f22e32d83da63cf307
AECX <maurice.henke@protonmail.com>
2021-02-11 00:04:49 +0100

    Hello world example code with _BUILDID constant

--- GameCube/source/main.cpp
+++ GameCube/source/main.cpp
@@ -9,9 +9,15 @@ namespace mod
 {
     void main()
     {
+        // Enable the console and set the individual lines to be shown
         libtp::tp::jfw_system::systemConsole->consoleEnabled = true;
         libtp::tp::jfw_system::systemConsole->consoleLine[0].showLine = true;
-        strcpy( libtp::tp::jfw_system::systemConsole->consoleLine[0].line, "Hello World" );
+        libtp::tp::jfw_system::systemConsole->consoleLine[1].showLine = true;
+
+        // Copy text data into the console line buffer
+        strcpy( libtp::tp::jfw_system::systemConsole->consoleLine[0].line, "Build: " );
+        strcpy( &libtp::tp::jfw_system::systemConsole->consoleLine[0].line[7], _BUILDID );
+        strcpy( libtp::tp::jfw_system::systemConsole->consoleLine[1].line, "Hello World" );
         return;
     }
 }     // namespace mod
