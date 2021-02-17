f44a59d0cc6f2e94a21e63de25874dd7ee9242de
Maurice <maurice.henke@protonmail.com>
2021-02-17 16:53:44 +0100

    Update libtp_rel

--- GameCube/source/main.cpp
+++ GameCube/source/main.cpp
@@ -5,7 +5,6 @@
 
 #include "display/console.h"
 #include "patch.h"
-#include "patches/changeFieldModels.h"
 
 namespace mod
 {
@@ -13,8 +12,6 @@ namespace mod
     {
         libtp::display::Console cout {};
 
-        libtp::patches::changeFieldModels();
-
-        cout << "Field models have been patched!\n";
+        cout << "Hello world";
     }
 }     // namespace mod
