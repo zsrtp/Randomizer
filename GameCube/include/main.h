85ecd6b0b235a3f975e263d75f2cb9093fa7d2e8
Maurice <maurice.henke@protonmail.com>
2021-04-05 19:04:31 +0200

    Example IntervalTimer

--- GameCube/include/main.h
+++ GameCube/include/main.h
@@ -1,4 +1,8 @@
+#include <chronos/timer.h>
 namespace mod
 {
+    libtp::chronos::IntervalTimer* t = nullptr;
+    uint32_t counter;
     void main();
-}
+    void ( *return_fapGm_Execute )() = nullptr;
+}     // namespace mod
--- GameCube/source/main.cpp
+++ GameCube/source/main.cpp
@@ -1,23 +1,36 @@
 #include "main.h"
 
+#include <cinttypes>
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
 
+#include "chronos/timer.h"
+#include "patch.h"
 #include "tp/JFWSystem.h"
+#include "tp/f_ap_game.h"
 
 namespace mod
 {
+    using namespace libtp::chronos;
     void main()
     {
-        // Enable the console and set the individual lines to be shown
-        libtp::tp::jfw_system::systemConsole->consoleEnabled = true;
-        libtp::tp::jfw_system::systemConsole->consoleLine[0].showLine = true;
-        libtp::tp::jfw_system::systemConsole->consoleLine[1].showLine = true;
+        // Initialise rando-system
+        return_fapGm_Execute = libtp::patch::hookFunction( libtp::tp::f_ap_game::fapGm_Execute, []() {
+            mod::t->tick();
 
-        // Copy text data into the console line buffer
-        strcpy( libtp::tp::jfw_system::systemConsole->consoleLine[0].line, "Build: " );
-        strcpy( &libtp::tp::jfw_system::systemConsole->consoleLine[0].line[7], _BUILDID );
-        strcpy( libtp::tp::jfw_system::systemConsole->consoleLine[1].line, "Hello World" );
+            mod::return_fapGm_Execute();
+        } );
+
+        libtp::tp::jfw_system::systemConsole->consoleEnabled = true;
+
+        t = new IntervalTimer( 120, []( Timer* timer ) {
+            libtp::tp::jfw_system::systemConsole->consoleLine[timer->m_ExecCount].showLine = true;
+            sprintf( libtp::tp::jfw_system::systemConsole->consoleLine[timer->m_ExecCount].line,
+                     "Timer Handler #" PRId32,
+                     timer->m_ExecCount );
+        } );
+
         return;
     }
 }     // namespace mod
