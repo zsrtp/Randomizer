dce904ee85f4b3a52c7e661e80593c725bc61e02
Maurice <maurice.henke@protonmail.com>
2021-07-03 22:29:02 +0200

    Added gamestates (boot, title, game)

--- GameCube/include/main.h
+++ GameCube/include/main.h
@@ -12,20 +12,27 @@
 
 #include "display/console.h"
 #include "rando/randomizer.h"
-#include "rando/seedselector.h"
+
+// Game States
+#define GAME_BOOT 0       // Default; At startup (should only ever be active once)
+#define GAME_TITLE 1      // When user is on title screen; let them choose their seed etc.
+#define GAME_ACTIVE 2     // When user is ingame and rando should be active
 
 namespace mod
 {
-    extern libtp::display::Console* console;
-    extern rando::SeedSelector* seedSelector;
+    // General public objects
+    extern libtp::display::Console console;
     extern rando::Randomizer* randomizer;
 
     // Variables
     extern uint32_t lastButtonInput;
     extern bool consoleState;
+    extern uint8_t gameState;
 
     void main();
     void hookFunctions();
+
+    void handleInput( uint32_t inputs );
 
     // Function hook handlers & trampolines
     void handle_fapGm_Execute( void );
--- GameCube/source/main.cpp
+++ GameCube/source/main.cpp
@@ -6,60 +6,97 @@
 #include <cstring>
 
 #include "patch.h"
+#include "tp/d_com_inf_game.h"
 #include "tp/f_ap_game.h"
 #include "tp/m_do_controller_pad.h"
 
 namespace mod
 {
     // Bind extern global variables
-    libtp::display::Console* console = new libtp::display::Console();
-    rando::SeedSelector* seedSelector = new rando::SeedSelector();
+    libtp::display::Console console;
     rando::Randomizer* randomizer = nullptr;
 
+    // Variables
+    uint32_t lastButtonInput = 0;
     bool consoleState = true;
+    uint8_t gameState = GAME_BOOT;
+
+    // Function hook return trampolines
     void ( *return_fapGm_Execute )( void ) = nullptr;
 
-    uint32_t lastButtonInput = 0;
-
-    void main() { hookFunctions(); }
+    void main()
+    {
+        // Just hook functions for now
+        hookFunctions();
+    }
 
     void hookFunctions()
     {
         // Hook functions
         return_fapGm_Execute = libtp::patch::hookFunction( libtp::tp::f_ap_game::fapGm_Execute, mod::handle_fapGm_Execute );
-        *console << "Functions have been hooked\n";
+        console << "Functions have been hooked\n";
     }
 
-    // TODO: Depending on the current state of the game we want this function to behave differently; i.e. there is no
-    // seedselector (required) when we're ingame; instead we will have the Randomizer
+    void handleInput( uint32_t input )
+    {
+        // Cycle through available seeds
+        if ( gameState == GAME_TITLE )
+        {
+            // TODO: Printing the currently selected seed
+        }
+    }
+
     void handle_fapGm_Execute()
     {
         using namespace libtp;
         using namespace tp::m_do_controller_pad;
-        uint32_t buttonInput = tp::m_do_controller_pad::cpadInfo.buttonInput;
+        using namespace tp::d_com_inf_game;
+
+        // Load relevant pointers locally for faster access
+        CPadInfo* padInfo = &cpadInfo;
+        GameInfo* gameInfo = &dComIfG_gameInfo;
+
+        // Handle game state updates
+        if ( 0 == strcmp( "F_SP102", gameInfo->currentStage ) || 0 == strcmp( "S_MV000", gameInfo->currentStage ) )
+        {
+            if ( gameState != GAME_TITLE )
+            {
+                // Title screen or demo
+                gameState = GAME_TITLE;
+                console << "State switched to TITLE\n";
+            }
+        }
+        else if ( gameState != GAME_ACTIVE && gameState != GAME_BOOT )
+        {
+            // We can't possibly be ingame if state was GAME_BOOT
+            gameState = GAME_ACTIVE;
+            console << "State switched to GAMEPLAY\n";
+        }
 
         // handle button inputs only if buttons are being held that weren't held last time
-        if ( buttonInput && buttonInput != lastButtonInput )
+        if ( padInfo->buttonInput != lastButtonInput )
         {
-            if ( ( buttonInput & ( PadInputs::Button_R | PadInputs::Button_Z ) ) ==
+            // Store before processing since we (potentially) un-set the padInfo values later
+            lastButtonInput = padInfo->buttonInput;
+
+            // Special combo to (de)activate the console should be handled first
+            if ( ( padInfo->buttonInput & ( PadInputs::Button_R | PadInputs::Button_Z ) ) ==
                  ( PadInputs::Button_R | PadInputs::Button_Z ) )
             {
                 consoleState = !consoleState;
                 display::setConsole( consoleState, 0 );
             }
-
-            // Handle Inputs if console is active
-            if ( consoleState )
+            // Handle Inputs if console is already active
+            else if ( consoleState )
             {
-                if ( seedSelector->HandleInputs( buttonInput ) )
-                {
-                    // print information
-                    *console << seedSelector->m_StatusText << "\n";
-                }
+                handleInput( padInfo->buttonInput );
+
+                // Disable input so game doesn't notice
+                padInfo->buttonInput = 0;
+                padInfo->buttonInputTrg = 0;
             }
         }
 
-        lastButtonInput = buttonInput;
         return return_fapGm_Execute();
     }
 }     // namespace mod
