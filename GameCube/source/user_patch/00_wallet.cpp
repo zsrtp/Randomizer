a0929ffe93c6346fa2ac8bec0b7cff5068e43ea2
lunarsoap5 <justindakotacarter@gmail.com>
2022-05-24 15:51:36 -0600

    Fixed bug with wallet patch causing wrong wallet icon to show up on pause menu

--- GameCube/source/user_patch/00_wallet.cpp
+++ GameCube/source/user_patch/00_wallet.cpp
@@ -40,11 +40,11 @@ namespace mod::user_patch
         *reinterpret_cast<uint32_t*>( getRupeeMaxPtr + 0x40 ) = ASM_LOAD_IMMEDIATE( 3, values[2] );
 
         // 600 -> 5000
-        *reinterpret_cast<uint32_t*>( setWalletMaxNumPtr + 0x18 ) = ASM_COMPARE_WORD_IMMEDIATE( 0, values[0] );
+        *reinterpret_cast<uint32_t*>( setWalletMaxNumPtr + 0x18 ) = ASM_COMPARE_WORD_IMMEDIATE( 0, values[1] );
         // 300 -> 1000
-        *reinterpret_cast<uint32_t*>( setWalletMaxNumPtr + 0x24 ) = ASM_COMPARE_WORD_IMMEDIATE( 0, values[1] );
+        *reinterpret_cast<uint32_t*>( setWalletMaxNumPtr + 0x24 ) = ASM_COMPARE_WORD_IMMEDIATE( 0, values[0] );
         // 1000 -> 9999
-        *reinterpret_cast<uint32_t*>( setWalletMaxNumPtr + 0x30 ) = ASM_COMPARE_WORD_IMMEDIATE( 0, values[1] );
+        *reinterpret_cast<uint32_t*>( setWalletMaxNumPtr + 0x30 ) = ASM_COMPARE_WORD_IMMEDIATE( 0, values[2] );
 
         // Update the cache
         gc_wii::os_cache::DCFlushRange( reinterpret_cast<void*>( getRupeeMaxPtr ), 0x41 );
