00d606745e764f1300c786be40f8c282b93ed5dc
Zephiles <cae222@yahoo.com>
2022-06-12 10:53:19 -0500

    Add clarification message when selecting seed

--- GameCube/source/main.cpp
+++ GameCube/source/main.cpp
@@ -212,6 +212,7 @@ namespace mod
                 console.setLine( 8 );
                 console << "\r"
                         << "Press X/Y to select a seed\n"
+                        << "Press R + Z to close the console\n"
                         << "[" << seedList->m_selectedSeed + 1 << "/" << static_cast<int32_t>( seedList->m_numSeeds )
                         << "] Seed: " << seedList->m_seedInfo[seedList->m_selectedSeed].header.seed << "\n";
             }
