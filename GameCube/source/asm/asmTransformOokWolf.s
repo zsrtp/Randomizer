3b4c27e66380a5c6848e571dafdfa2baa53c78d4
lunarsoap5 <justindakotacarter@gmail.com>
2022-06-13 00:00:35 -0500

    Fixed stack shifting and respective offsets

--- GameCube/source/asm/asmTransformOokWolf.s
+++ GameCube/source/asm/asmTransformOokWolf.s
@@ -2,19 +2,19 @@
 
 asmTransformOokWolf:
 # Push stack
-stwu %sp,-0x20(%sp)
+stwu %sp,-0x10(%sp)
 mflr %r0
 stw %r0,0x24(%sp)
-stw %r3,0xC(%sp)
-stw %r4,0x10(%sp)
+stw %r3,0x8(%sp)
+stw %r4,0xC(%sp)
 
 bl handleTransformFromWolf
 
 li %r5, 5
 # Pop stack
-lwz %r4,0x10(%sp)
-lwz %r3,0xC(%sp)
+lwz %r4,0xC(%sp)
+lwz %r3,0x8(%sp)
 lwz %r0,0x24(%sp)
 mtlr %r0
-addi %sp,%sp,0x20
+addi %sp,%sp,0x10
 blr
