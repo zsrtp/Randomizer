f562dd35c22c906ffea554bb4f19dd96314717be
Zephiles <cae222@yahoo.com>
2023-01-19 19:16:29 -0600

    Add Wii support for the codehandler

--- GameCube/subrel/boot/include/codehandler.h
+++ GameCube/subrel/boot/include/codehandler.h
@@ -3,6 +3,14 @@
 
 #include <cstdint>
 
+// Set up a define for the start of the hardware registers
+// Set the name to a small amount of characters to avoid breaking formatting
+#ifdef PLATFORM_WII
+#define HRSA 0xCD
+#else
+#define HRSA 0xCC
+#endif
+
 namespace mod::codehandler
 {
     extern const uint8_t codehandler[];
